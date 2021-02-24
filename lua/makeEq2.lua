6e055da0a6a3b3f772809f2a076d3ade3c7f6d0b
Manfred Grabherr <manfred.grabherr@imbim.uu.se>
2021-02-24 15:42:34 +0100

    Added lua script to generate equation systems.

---
+++ lua/makeEq2.lua
@@ -0,0 +1,22 @@
+
+for i=1, 5 do
+	x = math.random(10)-5
+	y = math.random(10)-5
+
+	a = math.random(12)-6
+	b = math.random(12)-6
+
+	c = x*a + y*b
+
+	print(a .. "x+" .. b .. "y=" .. c)
+
+	a = math.random(12)-6
+	b = math.random(12)-6
+
+	c = x*a + y*b
+
+	print(a .. "x+" .. b .. "y=" .. c)
+	print("")
+	print("")
+end
+
